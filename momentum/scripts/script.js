118f996620fa33e7183983cd461d9c0d2c1baea9
Marie Serykh <marie.serykh@gmail.com>
2021-10-22 19:02:42 +0200

    feat: implement changing images background images (on time of day, by sliding)

--- momentum/scripts/script.js
+++ momentum/scripts/script.js
@@ -1,13 +1,20 @@
 const timeDisplay = document.querySelector('.js-time');
 const dateDisplay = document.querySelector('.js-date');
-const date = new Date();
 const nameDisplay = document.querySelector('.js-name-display');
 const nameInput = document.querySelector('.js-name-input');
 const greetingMessage = document.querySelector('.js-greeting-message');
+const sliderBtnBack = document.querySelector('.js-slider-btn-back');
+const sliderBtnForward = document.querySelector('.js-slider-btn-forward');
+
+const background = document.querySelector('.js-background');
+
+const date = new Date();
 const hours = date.getHours();
 const timeOfDayList = ['night', 'morning', 'afternoon', 'evening'];
 const timeOfDay = getTimeOfDay();
 const greetingText = `Good ${timeOfDay},`;
+
+let randomNum;
 
 function showTime() {
   const date = new Date();
@@ -58,12 +65,49 @@ function getLocalStorage() {
   }
 }
 
+function getRandomNum() {
+  const max = 20;
+  randomNum = Math.floor(Math.random() * max + 1);
+}
+
+function setBg() {
+  const bgNum = randomNum.toString().padStart(2, "0");
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+  img.addEventListener('load', () => {
+    background.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+  });
+}
+
+function getSlideNext() {
+  randomNum++;
+  if (randomNum === 21) {
+    randomNum = 1;
+  }
+  
+  setBg();
+}
+
+function getSlidePrev() {
+  randomNum--;
+  if (randomNum === 0) {
+    randomNum = 20;
+  }
+  
+  setBg();
+}
+
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 
 nameDisplay.addEventListener('click', changeName);
 nameInput.addEventListener('blur', displayName);
 
+sliderBtnBack.addEventListener('click', getSlidePrev);
+sliderBtnForward.addEventListener('click', getSlideNext);
+
 showTime();
 displayName();
 showGreeting();
+getRandomNum();
+setBg();
